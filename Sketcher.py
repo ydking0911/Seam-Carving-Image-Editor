e20445d4d478e3e2a9efc2b02adf324d4418d6a0
99 <91651719+9September@users.noreply.github.com>
2024-11-03 16:23:04 +0900

    Add files via upload
    seam carving realtime tracing

---
+++ Sketcher.py
@@ -0,0 +1,32 @@
+# Sketcher.py
+import cv2
+
+class Sketcher:
+    def __init__(self, windowname, dests, colors_func):
+        self.prev_pt = None
+        self.windowname = windowname
+        self.dests = dests
+        self.colors_func = colors_func
+        self.dirty = False
+        self.show()
+        cv2.setMouseCallback(self.windowname, self.on_mouse)
+
+    def show(self):
+        cv2.imshow(self.windowname, self.dests[0])
+        # If you want to show the mask in a separate window, uncomment the next line
+        # cv2.imshow(self.windowname + ": mask", self.dests[1])
+
+    # onMouse function for Mouse Handling
+    def on_mouse(self, event, x, y, flags, param):
+        pt = (x, y)
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.prev_pt = pt
+        elif event == cv2.EVENT_LBUTTONUP:
+            self.prev_pt = None
+
+        if self.prev_pt and flags & cv2.EVENT_FLAG_LBUTTON:
+            for dst, color in zip(self.dests, self.colors_func()):
+                cv2.line(dst, self.prev_pt, pt, color, 20)
+            self.dirty = True
+            self.prev_pt = pt
+            self.show()
